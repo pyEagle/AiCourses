615c1b6be5fa340e1301446f8f24bf304bed6678
techMeta <913296832@qq.com>
2026-06-01 11:18:23 +0800

    update: voice_wake_up

--- speechProcessing/voice_wake_up.py
+++ speechProcessing/voice_wake_up.py
@@ -14,34 +14,24 @@ class VoiceWakeUp:
     def get_current_time(self):
         return time.time()
     
-    def run(self):
-        print("=== 语音唤醒已启动 ===")
-        print(f"唤醒词：[{self.WAKE_WORD}]，超时时间：{self.MAX_INTERVAL}秒")
+    def check(self, input_text):
+        if not self.flag: # 未被唤醒
+            if input_text != self.WAKE_WORD:
+                return (False, 0)
     
-        while True:
-            input_text = input("你说：").strip()
-            if input_text == "exit":
-                print("系统退出！")
-                break
-    
-            if not input_text:
-                continue
-    
-            if not self.flag: # 未被唤醒
-                if input_text != self.WAKE_WORD:
-                    continue
-    
-                print("AI：在的，可以帮你什么")
+            self.date_timer = self.get_current_time()
+            self.flag = True
+
+            return (True, 1) # 初次被唤醒
+        else: # 已被唤醒
+            temp_time = self.get_current_time()
+            time_diff = temp_time - self.date_timer
+            if time_diff < self.MAX_INTERVAL:
                 self.date_timer = self.get_current_time()
-                self.flag = True
-            else: # 已被唤醒
-                temp_time = self.get_current_time()
-                time_diff = temp_time - self.date_timer
-                if time_diff < self.MAX_INTERVAL: # 两句话，间隔不应该太长
-                    print(f"AI：已收到指令 -> {input_text}")
-                    self.date_timer = self.get_current_time()
-                else:
-                    self.flag = False
-                    self.date_timer = None
-                    print("AI：先回去睡个回笼觉嘞")
+                
+                return (True, 2) # 有效交流状态
+            else: # 设置为唤醒状态
+                self.flag = False
+                self.date_timer = None
+                return (False, 0) 
 
