312572bd58544b3cf7af91546fcd79a685962296
techMeta <913296832@qq.com>
2026-06-02 13:51:05 +0800

    Update ONNX model paths and improve logging

--- speechProcessing/onnx2rknn.py
+++ speechProcessing/onnx2rknn.py
@@ -4,35 +4,46 @@ pip install rknn-toolkit2
 
 from rknn.api import RKNN
 
-ONNX_MODEL = "./onnx_output/model.onnx"
-RKNN_MODEL = "./whisper_rk3588.rknn"
+ONNX_MODEL = "./onnx_output/encoder_model.onnx"
+RKNN_MODEL = "./whisper_encoder.rknn"
 
-rknn = RKNN()
+rknn = RKNN(verbose=True)
 
-print("配置 RKNN 环境...")
+print("Config")
+
 rknn.config(
-    mean_values=[[0]],
-    std_values=[[255]],
-    target_platform='rk3588',
-    optimization_level=3
+    target_platform="rk3588"
 )
 
-print("加载 ONNX 模型...")
-ret = rknn.load_onnx(model=ONNX_MODEL)
-if ret != 0:
-    raise RuntimeError("ONNX加载失败")
+print("加载 ONNX")
 
-print("构建 RKNN 模型...")
+ret = rknn.load_onnx(
+    model=ONNX_MODEL,
+    inputs=["input_features"],
+    input_size_list=[
+        [1,128,3000]
+    ]
+)
+
+if ret != 0:
+    raise RuntimeError("加载onnx失败")
+
+print("Build")
+
 ret = rknn.build(
-    do_quantization=True,
-    dataset="./dataset.txt"   # 校准数据（必须）
+    do_quantization=False
 )
-if ret != 0:
-    raise RuntimeError("build失败")
 
-print("导出 RKNN 模型...")
-rknn.export_rknn(RKNN_MODEL)
+if ret != 0:
+    raise RuntimeError("构建失败")
+
+print("Export")
+
+ret = rknn.export_rknn(RKNN_MODEL)
+
+if ret != 0:
+    raise RuntimeError("输出失败")
 
 rknn.release()
 
-print("完成！RK3588模型已生成:", RKNN_MODEL)
+print("成功")
