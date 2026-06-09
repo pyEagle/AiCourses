2480e85fdc4ee2a39b17ea9434e871605ba3e273
techMeta <913296832@qq.com>
2026-06-09 10:35:55 +0800

    Refactor YoloClipFaissRetriever initialization and methods

--- computerVision/image_sim/image_sim.py
+++ computerVision/image_sim/image_sim.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-
 import cv2
 import faiss
 import torch
@@ -14,19 +13,13 @@ from ultralytics import YOLO
 from transformers import CLIPProcessor, CLIPModel
 
 class YoloClipFaissRetriever:
-    def __init__(self,yolo_weight="yolov8m.pt", clip_model="openai/clip-vit-base-patch32"):
-        print("加载已训练 YOLOv8 模型")
+    def __init__(self, yolo_weight="best007.pt", clip_model="openai/clip-vit-base-patch32"):
+        print("加载 YOLOv8 模型...")
         self.yolo = YOLO(yolo_weight)
-
-        if torch.cuda.is_available():
-            self.device = "cuda"
-        elif torch.backends.mps.is_available():
-            self.device = "mps"
-        else:
-            self.device = "cpu"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"运行设备: {self.device}")
 
-        print("加载 CLIP 模型")
+        print("加载 CLIP 模型...")
         self.clip_model = CLIPModel.from_pretrained(clip_model).to(self.device)
         self.clip_processor = CLIPProcessor.from_pretrained(clip_model, clean_up_tokenization_spaces=True)
         self.clip_model.eval()
@@ -69,6 +62,9 @@ class YoloClipFaissRetriever:
             inputs = {k: v.to(self.device) for k, v in inputs.items()}
             feat = self.clip_model.get_text_features(**inputs)
         else:
+            #if isinstance(image_or_text, Image.Image):
+            #    image_or_text = image_or_text.convert("RGB")
+            
             inputs = self.clip_processor(images=image_or_text, return_tensors="pt")
             inputs = {k: v.to(self.device) for k, v in inputs.items()}
             feat = self.clip_model.get_image_features(**inputs)
@@ -77,19 +73,27 @@ class YoloClipFaissRetriever:
         return feat.cpu().numpy().astype("float32")
 
     def build_index(self, image_paths):
-        print(f"构建 FAISS 药品库索引，共 {len(image_paths)} 张图片...")
+        print(f"构建 FAISS 索引，共 {len(image_paths)} 张图片...")
         embeddings = []
         valid_paths = []
 
+        count, failed = 0, []
         for p in image_paths:
             crops = self.get_all_crops(p, conf_thresh=0.25)
             if crops:
-                emb = self.extract_feature(crops[0])
-                embeddings.append(emb)
-                valid_paths.append(p)
+                try:
+                    emb = self.extract_feature(crops[0])
+                    embeddings.append(emb)
+                    valid_paths.append(p)
+                except Exception as e:
+                    count += 1
+                    failed.append(p)
+                    #print('特征提取失败: ', p)
+        print(f'构建失败，共计{count}张:')
+        print('failed: '+'\n'.join(failed))
 
         if not embeddings:
-            print("没有提取到任何有效特征！")
+            print("未提取到有效特征！")
             return
 
         embeddings = np.vstack(embeddings)
@@ -106,36 +110,22 @@ class YoloClipFaissRetriever:
         if is_image_path:
             print(f"正在分析查询图片: {query}")
             crops = self.get_all_crops(query, conf_thresh=0.5)
-            print(f"发现 {len(crops)} 个有效药品目标。")
+            print(f"发现 {len(crops)} 个有效目标。")
             for i, crop in enumerate(crops):
                 q_feat = self.extract_feature(crop)
                 scores, idxs = self.index.search(q_feat, topk)
-
-                target_matches = []
-                for score, idx in zip(scores[0], idxs[0]):
-                    if idx != -1:
-                        target_matches.append((self.image_paths[idx], float(score)))
-                final_results.append({
-                    "target_id": i + 1,
-                    "matches": target_matches
-                })
+                target_matches = [(self.image_paths[idx], float(score)) for score, idx in zip(scores[0], idxs[0]) if idx != -1]
+                final_results.append({"target_id": i + 1, "matches": target_matches})
         else:
-            print(f"\n🔍 正在通过 [文本] 检索: '{query}'")
+            print(f"正在通过 [文本] 检索: '{query}'")
             q_feat = self.extract_feature(str(query))
             scores, idxs = self.index.search(q_feat, topk)
-
-            target_matches = []
-            for score, idx in zip(scores[0], idxs[0]):
-                if idx != -1:
-                    target_matches.append((self.image_paths[idx], float(score)))
-            final_results.append({
-                "target_id": "Text Query",
-                "matches": target_matches
-            })
+            target_matches = [(self.image_paths[idx], float(score)) for score, idx in zip(scores[0], idxs[0]) if idx != -1]
+            final_results.append({"target_id": "Text Query", "matches": target_matches})
 
         return final_results
 
-def main():
+def test():
     dir_name = sys.argv[1]
     query_input = sys.argv[2]
 
@@ -147,14 +137,13 @@ def main():
 
     retriever = YoloClipFaissRetriever()
     retriever.build_index(gallery)
-
     results = retriever.search(query_input, topk=1)
 
-    print("\n最终药品匹配结果:")
+    print("最终匹配结果:")
     for res in results:
-        print(f"\n[检出目标 {res['target_id']}] 最可能对应的库内药品:")
+        print(f"[检出目标 {res['target_id']}] 最可能对应的库内药品:")
         for path, score in res['matches']:
             print(f"库图路径: {path}  |  相似度: {score:.4f}")
 
 if __name__ == "__main__":
-    main()
+    test()
